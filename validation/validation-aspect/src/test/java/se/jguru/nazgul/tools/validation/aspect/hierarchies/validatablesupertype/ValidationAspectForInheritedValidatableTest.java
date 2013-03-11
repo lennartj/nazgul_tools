8d5986e1067d82adee7730c46b1580cd9fbe57de
Lennart Jörelid <lj@jguru.se>
2013-03-11 09:18:13 +0100

    Enforced license headers and license text using the maven-license-plugin.

--- validation/validation-aspect/src/test/java/se/jguru/nazgul/tools/validation/aspect/hierarchies/validatablesupertype/ValidationAspectForInheritedValidatableTest.java
+++ validation/validation-aspect/src/test/java/se/jguru/nazgul/tools/validation/aspect/hierarchies/validatablesupertype/ValidationAspectForInheritedValidatableTest.java
@@ -1,6 +1,23 @@
 /*
- * Copyright (c) jGuru Europe AB.
- * All rights reserved.
+ * #%L
+ * nazgul-tools-validation-api
+ * %%
+ * Copyright (C) 2010 - 2013 jGuru Europe AB
+ * %%
+ * Licensed under the jGuru Europe AB license (the "License"), based
+ * on Apache License, Version 2.0; you may not use this file except
+ * in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *        http://www.jguru.se/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
  */
 package se.jguru.nazgul.tools.validation.aspect.hierarchies.validatablesupertype;
 
